59559c987ea39017d8c2bde4bbaf029da6122420
Dr. Ralf Berger <dr.ralf.berger@gmail.com>
2017-07-31 17:11:04 +0200

    First webpack enabled version.

--- srcs/js/app/app.js
+++ srcs/js/app/app.js
@@ -1,13 +1,13 @@
-var app = new Vue(
-        {
-          el: '#app',
-          data: {
-            message: 'Hello Vue!',
-          },
-          methods: {
-            sayHello: () => {
-              alert("Submitting " + app.message)
-            }
-          }
+import hello from '../hello/hello'
+
+new Vue({
+    el: '#app',
+    data: {
+        message: 'Hello Vue!',
+    },
+    methods: {
+        sayHello: () => {
+            alert("Submitting " + app.message)
         }
-      )
+    }
+})
--- srcs/js/hello/hello.js
+++ srcs/js/hello/hello.js
@@ -1,6 +1,6 @@
 /**
  * An example for an very basic vue component, which works like a HTML component in the application.
  */
-var helloComponent = Vue.component('hello', {
+Vue.component('hello', {
     template: '<div class="w3-bar-block w3-center w3-blue w3-hover-indigo">Hello Vue Component!</div>'
 })
