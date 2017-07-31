c3e37bf01dd7bed1538451db3c6b15af5f7e8070
Dr. Ralf Berger <dr.ralf.berger@gmail.com>
2017-07-31 17:21:43 +0200

    Including watching the build.

--- srcs/js/hello/hello.js
+++ srcs/js/hello/hello.js
@@ -2,5 +2,5 @@
  * An example for an very basic vue component, which works like a HTML component in the application.
  */
 Vue.component('hello', {
-    template: '<div class="w3-bar-block w3-center w3-blue w3-hover-indigo">Hello Vue Component!</div>'
+    template: '<div class="w3-bar-block w3-center w3-blue w3-hover-indigo">Hello Vue Component with webpack!</div>'
 })
