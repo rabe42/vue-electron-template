b74af22e6784a26d353b580078eb13301748acda
Dr. Ralf Berger <dr.ralf.berger@gmail.com>
2017-08-01 17:31:41 +0200

    Some janitors work.

--- srcs/js/hello/hello.vue
+++ srcs/js/hello/hello.vue
@@ -1,5 +1,4 @@
 <!-- An example for an very basic vue component, which works like a HTML component in the application. -->
-<!-- TODO: webpack don't create a component from this. Maybe the name is the issue?  -->
 <template>
     <div class="w3-bar-block w3-center w3-blue w3-hover-indigo">Hello Vue Component with webpack!</div>
 </template>
