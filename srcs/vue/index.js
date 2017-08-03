f6356682931e95ec76d01e21ccfd6ed081c6409b
Dr. Ralf Berger <dr.ralf.berger@gmail.com>
2017-08-03 17:00:06 +0200

    First assets are provided in the dist folder.

--- srcs/vue/index.js
+++ srcs/vue/index.js
@@ -1,3 +1,8 @@
+// Make the resources of the front end available.
+require("file-loader?name=[name].[ext].[hash]!../../index.html")
+require("file-loader?name=[name].[ext]!w3-css/w3.css")
+
+// Import the framework and the app
 import Vue from 'vue'
 import app from './app/app'
 
