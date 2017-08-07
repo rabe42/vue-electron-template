79211d9239ba4a2392f4afd84776f5e7dbce7dee
Dr. Ralf Berger <dr.ralf.berger@gmail.com>
2017-08-07 21:26:09 +0200

    Correct application of css with webpack.

--- srcs/vue/index.js
+++ srcs/vue/index.js
@@ -1,8 +1,9 @@
 // Make the resources of the front end available.
 require("file-loader?name=[name].[ext]!../../index.html")
-require("file-loader?name=[name].[ext]!w3-css/w3.css")
-require("font-awesome-webpack")
-//require("file-loader?name=[name].[ext]!")
+
+// Integrates the style sheet directly into the dom.
+require("!style-loader!css-loader!w3-css/w3.css")
+//require("font-awesome-webpack")
 
 // Import the framework and the app
 import Vue from 'vue'
