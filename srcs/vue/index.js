7631918044fd89fc2d006d2ed66f54da7b5724b6
Dr. Ralf Berger <dr.ralf.berger@gmail.com>
2017-08-19 19:50:08 +0200

    ES6 imports instead of require()

--- srcs/vue/index.js
+++ srcs/vue/index.js
@@ -1,9 +1,9 @@
 // Make the resources of the front end available.
-require("file-loader?name=[name].[ext]!../../index.html")
+import 'file-loader?name=[name].[ext]!../../index.html'
 
 // Integrates the style sheet directly into the dom.
-require("w3-css/w3.css")
-require("font-awesome/css/font-awesome.min.css")
+import 'w3-css/w3.css'
+import 'font-awesome/css/font-awesome.min.css'
 
 // Import the framework and the app
 import Vue from 'vue'
