f606cfc9ffa1495d181922499097648354eed0ea
Dr. Ralf Berger <dr.ralf.berger@gmail.com>
2017-09-03 21:07:31 +0200

    Demonstration of the simple data component.

--- srcs/vue/hello/hello.vue
+++ srcs/vue/hello/hello.vue
@@ -1,6 +1,6 @@
 <!-- An example for an very basic vue component, which works like a HTML component in the application. -->
 <template>
-    <div class="w3-bar-block w3-center w3-blue w3-hover-indigo">Hello Vue Component with webpack!</div>
+    <div class="w3-bar-block w3-center w3-blue w3-hover-indigo">Hello {{ vueMessage }} with webpack!</div>
 </template>
 
 <<script>
@@ -8,6 +8,7 @@ export default {
   data () {
       return {
           // Component data goes here!
+          vueMessage: 'Vue component'
       }
   }
 }
