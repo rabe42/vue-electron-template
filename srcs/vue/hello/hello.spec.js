18f53f90912523aba7b4e62aa26420d436e086e7
Dr. Ralf Berger <dr.ralf.berger@gmail.com>
2017-09-09 11:35:10 +0200

    First test written.

---
+++ srcs/vue/hello/hello.spec.js
@@ -0,0 +1,14 @@
+import vue from 'Vue'
+import Hello from 'hello'
+
+describe('Hello component', () => {
+    it('has a created block', () => {
+        expect(typeof Hello.created).toBe('function')
+    })
+
+    it('has a vueMessage in data', () => {
+        expect(typeof Hello.data).toBe('function')
+        let defaultData = Hello.data()
+        expect(defaultData.vueMessage).toBe('Vue component')
+    })
+})
