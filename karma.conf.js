c220cf91a5ce0827de7bf79d23eb5ad7e65d7b70
Dr. Ralf Berger <dr.ralf.berger@gmail.com>
2017-10-08 10:54:25 +0200

    Introducing karma into the game.

---
+++ karma.conf.js
@@ -0,0 +1,36 @@
+//
+// This is the karma configuration file, providing unit test capabilities for vue via webpack.
+//
+
+const webpackConfig = require(__dirname + '/webpack.config.js') // FIXME: Not possible to read the configuration.
+
+module.exports = function (config) {
+  config.set({
+    // to run in additional browsers:
+    // 1. install corresponding karma launcher
+    //    http://karma-runner.github.io/0.13/config/browsers.html
+    // 2. add it to the `browsers` array below.
+    browsers: ['PhantomJS'],
+    frameworks: ['jasmine'],
+    reporters: ['spec', 'coverage'],
+    files: [
+      './srcs/index.js',
+      {pattern: 'srcs/**/*.spec.js'} // each .spec.js file acts as entry point for the webpack configuration
+    ],
+    preprocessors: {
+      './srcs/index.js': ['webpack', 'sourcemap'],  // This should run the given file through webpack (I hope!)
+      './srcs/**/*.spec.js': ['webpack', 'sourcemap']
+    },
+    webpack: webpackConfig, // This should trigger the webpack process for the test.
+    webpackMiddleware: {
+      noInfo: true
+    },
+    coverageReporter: {
+      dir: './coverage',
+      reporters: [
+        { type: 'lcov', subdir: '.' },
+        { type: 'text-summary' }
+      ]
+    }
+  })
+}
---
+++ webpack.test.config.js
@@ -0,0 +1,87 @@
+var path = require('path')
+var webpack = require('webpack')
+
+module.exports = {
+  target: 'electron',
+  entry: './srcs/vue/index.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    publicPath: 'dist/',
+    filename: 'test.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: {
+          loaders: {
+          }
+          // other vue-loader options go here
+        }
+      },
+      { 
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]?[hash]'
+        }
+      },
+      // For support of font-awesome
+      { 
+        test: /\.(woff|woff2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, 
+        loader: 'url-loader',
+        options: {
+          limit: 100000, /* Making sure, that the font is embedded! */
+          mimetype: 'application/font-woff'
+        }
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'file-loader',
+        options: {
+          publicPath: 'dist'
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.vue', '.json'],
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js'
+    }
+  },
+  devServer: {
+    historyApiFallback: true,
+    noInfo: true
+  },
+  performance: {
+    hints: false
+  },
+  devtool: '#eval-source-map'
+}
+
+if (process.env.NODE_ENV === 'production') {
+  module.exports.devtool = '#source-map'
+  // http://vue-loader.vuejs.org/en/workflow/production.html
+  module.exports.plugins = (module.exports.plugins || []).concat([
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"'
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: true,
+      compress: {
+        warnings: false
+      }
+    }),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true
+    })
+  ])
+}
